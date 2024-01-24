90523cc8b688adc09aa3d5883fe0c0c39b76b0f7
BRINTHA1102 <brinthalingam@gmail.com>
2024-01-24 10:19:20 +0530

    loginactivity and managaeaccount component created and html css has been added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbDropdown, NgbDropdownModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home/home.component'; 
+import { LoginactivityComponent } from './loginactivity/loginactivity.component';
+import { ManageaccountComponent } from './manageaccount/manageaccount.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginactivityComponent,
+    ManageaccountComponent
   ],
   imports: [
     BrowserModule,
