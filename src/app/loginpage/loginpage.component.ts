f6bcaa8905b136b395146fb3d7ad89ba42415622
unknown <arunkumar101501@gmail.com>
2024-01-24 17:10:50 +1300

    routing modified

--- src/app/loginpage/loginpage.component.ts
+++ src/app/loginpage/loginpage.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-loginpage',
@@ -8,21 +6,5 @@ import { Router } from '@angular/router';
   styleUrl: './loginpage.component.css'
 })
 export class LoginpageComponent {
-  loginForm!: FormGroup;
 
-  constructor(private fb: FormBuilder,private router:Router) {}
-
-  ngOnInit() {
-    this.loginForm = this.fb.group({
-      userId: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-    });
-  }
-
-  onSubmit() {
-      // Implement your form submission logic here
-      console.log('Form submitted:', this.loginForm.value);
-      this.router.navigateByUrl('/home');
-    
-  }
 }
