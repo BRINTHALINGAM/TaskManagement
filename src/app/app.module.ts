e27ddb6e9259e15229f66b48255aac7689b3d12f
kaviya <kaviyakavi55667@gmail.com>
2024-01-24 10:12:55 +0530

    Acl settings added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbDropdown, NgbDropdownModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
-<<<<<<< HEAD
-import { AclSettingsComponent } from './acl-settings/acl-settings.component';
-=======
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
->>>>>>> 6d686e4c25c5815cf2bac63036bbcf46c7c90294
+import { AclSettingsComponent } from './acl-settings/acl-settings.component';
 
 @NgModule({
   declarations: [
