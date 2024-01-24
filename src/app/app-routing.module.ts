97ded9ca969b14c88caa94c2421995fdb2c46e17
Gurupriya K <gurupriyakumar054@gmail.com>
2024-01-24 11:27:11 +0530

    Guru commited

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { LoginactivityComponent } from './loginactivity/loginactivity.component'
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ManageaccountComponent } from './manageaccount/manageaccount.component';
+import { ChartComponent } from './chart/chart.component';
 
 
 const routes: Routes = [
   { path:'loginactivity',component:LoginactivityComponent },
   {path: 'manageaccount', component:ManageaccountComponent}
 
-
+  
 ];
 
 @NgModule({
