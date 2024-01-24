6f22c1bde197ef87d3570622f4b8924c2e9e2e1e
kanishkasri16 <kanishkaselvan003@gmailcom>
2024-01-24 11:50:38 +0530

    Team C components created

---
+++ src/app/Team_C/imageuploader/imageuploader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imageuploader',
+  templateUrl: './imageuploader.component.html',
+  styleUrl: './imageuploader.component.css'
+})
+export class ImageuploaderComponent {
+
+}
