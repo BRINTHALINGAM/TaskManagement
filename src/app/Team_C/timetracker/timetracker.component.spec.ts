3fd9f1db8b4c3ab1c73696c896fcce9097c00989
Vishnupg06 <vishnupg1912@gmail.com>
2024-01-25 11:33:47 +0530

    conflicts resolved

--- src/app/Team_C/timetracker/timetracker.component.spec.ts
+++ src/app/Team_C/timetracker/timetracker.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TimetrackerComponent } from './timetracker.component';
+import { TimeTrackingComponent } from './timetracker.component';
 
 describe('TimetrackerComponent', () => {
-  let component: TimetrackerComponent;
-  let fixture: ComponentFixture<TimetrackerComponent>;
+  let component: TimeTrackingComponent;
+  let fixture: ComponentFixture<TimeTrackingComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [TimetrackerComponent]
+      declarations: [TimeTrackingComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(TimetrackerComponent);
+    fixture = TestBed.createComponent(TimeTrackingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
